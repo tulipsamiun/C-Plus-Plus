3f261fee0347d0bb09af9d6f7052de0c1465939d
tulipsamiun <tsamiun_ts@yahoo.com>
2021-05-06 00:16:19 +0600

    Update primeNumbers.cpp

--- Algorithm and Data Structure/primeNumbers.cpp
+++ Algorithm and Data Structure/primeNumbers.cpp
@@ -9,31 +9,29 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include<iostream>
 #include<math.h>
 
-//include <stdio.h>
 int main()
 {
-    //printf("Hello World");
     int n;
     printf("Enter Value for n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     int primes[n+1];
     for(int i=0;i<n;i++){
-        primes[i]=1;
+        primes[i] = 1;
     }
-    primes[0]=0;
-    primes[1]=0;
-    for(int i=2; i<=sqrt(n);i++){
+    primes[0] = 0;
+    primes[1] = 0;
+    for(int i = 2; i <= sqrt(n); i++){
         if(primes[i]==1)
         {
-            for(int j=2;i*j<=n;j++){
+            for(int j = 2; i * j <= n; j++){
             primes[i*j]=0;
             }
         }
     }
     printf("Primes ");
-    for(int i=2; i<=n;i++){
-     if(primes[i]==1)
-        printf("%d ",i);
+    for(int i = 2; i <= n; i++){
+     if(primes[i] == 1)
+        printf("%d ", i);
     }
     return 0;
 }
