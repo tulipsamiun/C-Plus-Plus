5275f3a6005ad25ee844dca085621a81409e6a20
tulipsamiun <tsamiun_ts@yahoo.com>
2021-11-26 03:41:53 +0600

    Create uperToTitleCase.cpp

---
+++ Practice/uperToTitleCase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string text;
+    int i,n;
+    printf("Enter Uper Case Character : ");
+    getline(cin,text);
+    n = text.size();
+    text[0] = toupper(text[0]);
+    for( i = 1 ; i < n; i++){
+        if(text[i-1] == ' '){
+            text[i] = toupper(text[i]);
+        }
+        else 
+            text[i] =tolower(text[i]);
+    }
+    cout << text;
+    return 0;
+}
