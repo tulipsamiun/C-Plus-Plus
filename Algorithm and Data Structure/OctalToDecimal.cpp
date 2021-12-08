98c2dfd62a728e54a328c2dd91eeb497c27caf62
tulipsamiun <tsamiun_ts@yahoo.com>
2021-12-09 03:41:30 +0600

    Create OctalToDecimal.cpp

---
+++ Algorithm and Data Structure/OctalToDecimal.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    //printf("Hello World");
+    int dec =0 , n, i;
+    string oct;
+    printf("enter Number");
+    getline(cin,oct);
+    n = oct.size();
+    n = n-1;
+    for (i = n; i >= 0; i--){
+        dec = dec + ((oct[i]-48) * pow(8,n-i));
+    }
+    printf("Decimal = %d", dec);
+    return 0;
+}
