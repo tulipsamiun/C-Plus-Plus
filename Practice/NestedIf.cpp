93053305bc255bdb33043f8c616dc1e1c77f1902
tulipsamiun <tsamiun_ts@yahoo.com>
2021-12-13 01:43:24 +0600

    Create NestedIf.cpp

---
+++ Practice/NestedIf.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month, day;
+    printf("Enter Month and Date ");
+    scanf("%d%d", &month, &day);
+    if (month == 2){
+       if(day == 19)
+        printf("Happy Birthday to Taru");
+    }
+    return 0;
+}
