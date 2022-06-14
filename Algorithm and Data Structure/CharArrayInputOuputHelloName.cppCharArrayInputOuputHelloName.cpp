0fc2199b8a1e518789f3d5eb3259fcdb76846ea1
tulipsamiun <tsamiun_ts@yahoo.com>
2022-06-15 04:15:07 +0600

    Create CharArrayInputOuputHelloName.cppCharArrayInputOuputHelloName.cpp

---
+++ Algorithm and Data Structure/CharArrayInputOuputHelloName.cppCharArrayInputOuputHelloName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char name[20];
+    printf("Enter Your Name ");
+    //cin >> name;
+    scanf("%s", name);
+    printf("Hello ");
+    //cout << name;
+    printf("%s", name);
+
+    return 0;
+}
